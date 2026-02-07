9573c9c38425a03cfdab6d62f34e30e62640a3e3
LucasSalees <saleslucaas01@gmail.com>
2026-02-07 11:18:23 -0300

    REFACTOR: ajusta UX e UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,54 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Wind, Loader2 } from 'lucide-react'; //icones
 import api from './services/api';
 
 function App() {
-  const [agendamentos, setAgendamentos] = useState([]);
+
+  const [agendamentos, setAgendamentos] = useState ([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Função para buscar dados do seu backend no Render
-    api.get('/agendamentos')
-      .then(response => {
+    const fetchDados = async () => {
+      try {
+        const response = await api.get('/agendamentos');
         setAgendamentos(response.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error("Erro ao conectar com o backend:", error);
-        setLoading(false);
-      });
+      } catch (err) {
+        console.error("Erro ao buscar:", err);
+      } finally {
+        setLoading(false); //desliga o loading independente de sucesso ou não
+      }
+    };
+    fetchDados();
   }, []);
 
-  if (loading) return <div>Acordando o servidor no Render... ❄️</div>;
+  // tela de loading (UX)
+  if (loading) {
+    return (
+      <div className='h-screen w-full flex flex-col itens-center justify-center bg-slate-50'>
+        <Loader2 className='w-12 h-12 text-blue-500 animate-spin' />
+          <h2 className='mt-4 text-xl font-semibold text-slate-700'> Acordando o servidor... </h2>
+            <p className='text-slate-500'> Isso pode levar até um minuto </p>
+      </div>
+    )
+  }
 
+  //tela principal (UI)
   return (
-    <div>
-      <h1>Sistema Project Air Conditioning</h1>
-      <p>Agendamentos encontrados: {agendamentos.length}</p>
-      {/* Aqui listaremos os agendamentos do seu Supabase */}
+    <div className="min-h-screen bg-slate-100 p-4 md:p-8">
+      <header className="max-w-4xl mx-auto mb-8 flex items-center gap-3">
+        <Wind className="w-8 h-8 text-blue-600" />
+        <h1 className="text-2xl font-bold text-slate-800">Air System Control</h1>
+      </header>
+
+      <main className="max-w-4xl mx-auto">
+        {/* Aqui entrará o formulário e a lista */}
+        <div className="bg-white rounded-xl shadow-sm p-6 border border-slate-200">
+           <p className="text-slate-600">Servidor Online! Agendamentos: {agendamentos.length}</p>
+        </div>
+      </main>
     </div>
   );
+
 }
 
 export default App;
