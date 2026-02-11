419a40eebe4fa0edc58426b996f411e9b065bec6
LucasSalees <saleslucaas01@gmail.com>
2026-02-11 10:02:42 -0300

    REFACTOR:  Update Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 }); */}
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://project-air-conditioning.onrender.com'
+    baseURL: 'https://project-air-conditioning.onrender.com' 
 });
 
 // Adiciona o token em todas as chamadas automaticamente
